663e5d57a9a6bb70855d209440bb76c5d9248b67
Marc Antonio Queiroz <marc@xbrain.com.br>
2015-09-02 17:54:02 -0300

    Implementing functional interface for AriCallback. Now its possible to use lambda expressions, instead anonymous clause.

--- classes/ch/loway/oss/ari4java/tools/AriCallback.java
+++ classes/ch/loway/oss/ari4java/tools/AriCallback.java
@@ -8,7 +8,28 @@ package ch.loway.oss.ari4java.tools;
  * @param <T>
  * The type of result returned in callback
  */
+@FunctionalInterface
 public interface AriCallback<T> {
     void onSuccess(T result);
-    void onFailure(RestException e);
+    default void onFailure(RestException e) {
+      e.printStackTrace();
+    }
 }
+
+
+//To achieve this one can add two additional interfaces:
+//
+//public interface FutureCompletionCallback<V> {
+//    public void onSuccess(V result);
+//}
+//
+//public interface FutureFailureCallback {
+//    void onFailure(Throwable t);
+//}
+//
+//And the folling methods to Futures class:
+//
+//public static <V> void onComplete(ListenableFuture<V> future, FutureCompletionCallback<? super V> callback)
+//
+//public static <V> void onFailure(ListenableFuture<V> future, FutureFailureCallback callback)
+//
